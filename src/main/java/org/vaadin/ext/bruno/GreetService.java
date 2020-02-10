2af99a204411bdfa2900cc10628904e2c1da32c5
Bruno Rezende <brunovianarezende@gmail.com>
2020-02-10 15:12:08 -0300

    start of development

---
+++ src/main/java/org/vaadin/ext/bruno/GreetService.java
@@ -0,0 +1,12 @@
+package org.vaadin.ext.bruno;
+
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
